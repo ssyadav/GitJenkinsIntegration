d61e47e23b81cec7f737299ebd3cb229028955e7
satyaveer.yadav <satyaveer.yadav@therofisher.com>
2018-11-07 01:00:03 +0530

    git integration with jenkins

---
+++ JenkinIntegration.java
@@ -0,0 +1,18 @@
+
+/**
+ * @author satyaveer.yadav
+ *
+ */
+public class JenkinIntegration {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i = 0; i < 10; i++ ) {
+			System.out.println("Hello World " + i);
+		}
+	}
+
+}
